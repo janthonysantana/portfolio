66043dc6b3fd6da16aef95ac3d11fd8bd812c503
Josue Santana <joansanres@gmail.com>
2024-01-31 17:17:41 -0500

    Added tooltips to social media links in Footer component

--- src/nav/Footer.jsx
+++ src/nav/Footer.jsx
@@ -6,11 +6,11 @@ export const Footer = () => {
         <footer>
             <p>Reach out or follow me @</p>
             <ul class="footer-list">
-                <Link activeClassName='active' href='https://github.com/janthonysantana'><li><iconify-icon icon="mdi:github"></iconify-icon></li></Link>
-                <Link activeClassName='active' href='https://www.linkedin.com/in/anthony-santana-ny/'><li><iconify-icon icon="mdi:linkedin"></iconify-icon></li></Link>
-                <Link activeClassName='active' href='https://medium.com/@janthonysantana'><li><iconify-icon icon="ri:medium-fill"></iconify-icon></li></Link>
-                <Link activeClassName='active' href='https://mastodon.social/@jasantana'><li><iconify-icon icon="mdi:mastodon"></iconify-icon></li></Link>
-                <Link activeClassName='active' href='https://discord.com/users/jasantana_dev'><li><iconify-icon icon="ic:baseline-discord"></iconify-icon></li></Link>              
+                <Link activeClassName='active' href='https://github.com/janthonysantana'><li title="Github"><iconify-icon icon="mdi:github"></iconify-icon></li></Link>
+                <Link activeClassName='active' href='https://www.linkedin.com/in/anthony-santana-ny/'><li title="LinkedIn"><iconify-icon icon="mdi:linkedin"></iconify-icon></li></Link>
+                <Link activeClassName='active' href='https://medium.com/@janthonysantana'><li title="Medium"><iconify-icon icon="ri:medium-fill"></iconify-icon></li></Link>
+                <Link activeClassName='active' href='https://mastodon.social/@jasantana'><li title="Mastodon"><iconify-icon icon="mdi:mastodon"></iconify-icon></li></Link>
+                <Link activeClassName='active' href='https://discord.com/users/jasantana_dev'><li title="Discord"><iconify-icon icon="ic:baseline-discord"></iconify-icon></li></Link>              
             </ul>
         </footer>
     )
