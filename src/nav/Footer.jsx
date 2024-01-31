12a80f53a3030a1911e0bf0204ddd99909811b88
Josue Santana <joansanres@gmail.com>
2024-01-31 17:12:59 -0500

    Updated fonts and added links to footer

--- src/nav/Footer.jsx
+++ src/nav/Footer.jsx
@@ -1,15 +1,16 @@
 import './footer.css'
+import { Link } from 'preact-router/match'
 
 export const Footer = () => {
     return (
         <footer>
             <p>Reach out or follow me @</p>
             <ul class="footer-list">
-                <li><iconify-icon icon="mdi:github"></iconify-icon></li>
-                <li><iconify-icon icon="mdi:linkedin"></iconify-icon></li>
-                <li><iconify-icon icon="ri:medium-fill"></iconify-icon></li>
-                <li><iconify-icon icon="mdi:mastodon"></iconify-icon></li>
-                <li><iconify-icon icon="ic:baseline-discord"></iconify-icon></li>
+                <Link activeClassName='active' href='https://github.com/janthonysantana'><li><iconify-icon icon="mdi:github"></iconify-icon></li></Link>
+                <Link activeClassName='active' href='https://www.linkedin.com/in/anthony-santana-ny/'><li><iconify-icon icon="mdi:linkedin"></iconify-icon></li></Link>
+                <Link activeClassName='active' href='https://medium.com/@janthonysantana'><li><iconify-icon icon="ri:medium-fill"></iconify-icon></li></Link>
+                <Link activeClassName='active' href='https://mastodon.social/@jasantana'><li><iconify-icon icon="mdi:mastodon"></iconify-icon></li></Link>
+                <Link activeClassName='active' href='https://discord.com/users/jasantana_dev'><li><iconify-icon icon="ic:baseline-discord"></iconify-icon></li></Link>              
             </ul>
         </footer>
     )
